c7ec57ec58e45691fd8c8d2481dd3f7558c47aac
hugokernel <hugo@digitalspirit.org>
2014-12-25 17:07:55 +0100

    read_w1_temperature modification

--- functions.py
+++ functions.py
@@ -19,54 +19,6 @@ def speak(data):
 
     return data[0].format(*out)
 
-def read_w1_temperature(index, fahrenheit=False, maxretry=3, basedir='/sys/bus/w1/devices/'):
-    '''
-    Read temperature from 1Wire bus
-    Todo:
-     * Read 2 times and compares output
-     * Index arg as sensor id array (ex: index = [ '10-xxxxx', '28-XXXXxx' ])
-    '''
-
-    device_folder = glob.glob(basedir + '??-*')
-
-    def parse(data):
-        while not data or data[0].strip()[-3:] != 'YES':
-            return (False, 0)
-
-        equals_pos = data[1].find('t=')
-        if equals_pos != -1:
-            temp_string = data[1][equals_pos+2:]
-            temp_c = float(temp_string) / 1000.0
-            return (True, temp_c if not fahrenheit else temp_c * 9.0 / 5.0 + 32.0)
-
-    out = []
-    for item in index if type(index) == list else [ index ]:
-
-        retry = 0
-        while True:
-            lines = None
-            try:
-                with open(device_folder[item] + '/w1_slave', 'r') as file:
-                    lines = file.readlines()
-            except (IndexError, IOError):
-                out.append(None)
-                break
-
-            status, data = parse(lines)
-            if status:
-                out.append(data)
-                break
-
-            if retry == maxretry:
-                out.append(None)
-                break
-
-            time.sleep(0.2)
-
-            retry += 1
-
-    return out if not len(out) or type(index) == list else out[0]
-
 def elapsed_time(seconds, suffixes=['y','w','d','h','m','s'], add_s=False, separator=' '):
     """
     Takes an amount of seconds and turns it into a human-readable amount of time.
@@ -96,4 +48,68 @@ def elapsed_time(seconds, suffixes=['y','w','d','h','m','s'], add_s=False, separ
             break
      
     return separator.join(time)
+
+def read_w1_temperature(sensors, fahrenheit=False, maxretry=3, basedir='/sys/bus/w1/devices/'):
+    '''
+    Read temperature from 1Wire bus
+    Todo:
+     * Read 2 times and compares output
+    '''
+
+    device_folders = glob.glob(basedir + '??-*')
+
+    def parse(data):
+        while not data or data[0].strip()[-3:] != 'YES':
+            return (False, 0)
+
+        equals_pos = data[1].find('t=')
+        if equals_pos != -1:
+            temp_string = data[1][equals_pos+2:]
+            temp_c = float(temp_string) / 1000.0
+            return (True, temp_c if not fahrenheit else temp_c * 9.0 / 5.0 + 32.0)
+
+    def get_device_path(key):
+        if type(key) == int:
+            return device_folders[key]
+        elif type(key) == str:
+            for path in device_folders:
+                if path[-len(key):] == key:
+                    return path
+
+    out = []
+    for sensor in sensors if type(sensors) == list else [ sensors ]:
+
+        path = get_device_path(sensor)
+
+        retry = 0
+        while True:
+            lines = None
+            try:
+                with open(path + '/w1_slave', 'r') as file:
+                    lines = file.readlines()
+            except (IndexError, IOError):
+                out.append(None)
+                break
+
+            status, data = parse(lines)
+            if status:
+                out.append(data)
+                break
+
+            if retry == maxretry:
+                out.append(None)
+                break
+
+            time.sleep(0.2)
+
+            retry += 1
+
+    return out if not len(out) or type(sensors) == list else out[0]
+
+if __name__ == '__main__':
+    result = read_w1_temperature([0, 1, 2], basedir='/tmp/')
+    print result
+
+    result = read_w1_temperature(['10-0008008ba2a9', '28-0000061496ff', '10-0008008bceb5'], basedir='/tmp/')
+    print result
 
